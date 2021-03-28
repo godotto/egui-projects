c7baf16b23246fb5d87e9fdf7f242de71fab0628
mmarcin5 <m.marcinkiewicz.5@stud.elka.pw.edu.pl>
2021-03-28 23:16:54 +0200

    Create visuals for the main window

--- lab1/recipe-planner/recipeplanner.cpp
+++ lab1/recipe-planner/recipeplanner.cpp
@@ -3,9 +3,81 @@
 RecipePlanner::RecipePlanner(QWidget *parent)
     : QMainWindow(parent)
 {
+    // set window size
+    setMinimumSize(400, 330);
+    resize(600, 500);
+
+    // set window title
+    setWindowTitle("Recipe Planner");
+
+    // central widget definition
+    m_centralWidget = new QWidget(this);
+    setCentralWidget(m_centralWidget);
+
+    // list defiinition
+    m_recipeList = new QListWidget(m_centralWidget);
+
+    createButtons();
+    createLayouts();
+    createActions();
+    createMenu();
+
+    // set QWidget with content as central widget
+    m_centralWidget->setLayout(m_mainLayout);
 }
 
-RecipePlanner::~RecipePlanner()
+void RecipePlanner::createButtons()
 {
+    // buttons definition
+    m_addButton = new QPushButton("&Add", m_centralWidget);
+    m_editButton = new QPushButton("&Edit", m_centralWidget);
+    m_deleteButton = new QPushButton("&Delete", m_centralWidget);
+
+    // set tooltips
+    m_addButton->setToolTip("Add new recipe");
+    m_editButton->setToolTip("Edit selected recipe");
+    m_deleteButton->setToolTip("Delete selected recipes");
 }
 
+void RecipePlanner::createLayouts()
+{
+    // layouts definition
+    m_mainLayout = new QHBoxLayout(m_centralWidget);
+    m_buttonLayout = new QVBoxLayout();
+
+    m_buttonLayout->setAlignment(Qt::AlignTop); // move buttons to the top of right pane
+    m_buttonLayout->setSpacing(15);             // make some space between buttons
+
+    // add buttons to button layout
+    m_buttonLayout->addWidget(m_addButton);
+    m_buttonLayout->addWidget(m_editButton);
+    m_buttonLayout->addWidget(m_deleteButton);
+
+    // add list and button layout to the main layout
+    m_mainLayout->addWidget(m_recipeList);
+    m_mainLayout->addLayout(m_buttonLayout);
+}
+
+void RecipePlanner::createActions()
+{
+    // actions definition
+    m_createMenuAction = new QAction("&Create menu", this);
+    m_exitAction = new QAction("&Exit", this);
+
+    // set actions status tips
+    m_createMenuAction->setStatusTip("Create menu from selected recipes");
+
+    // add shortcuts for actions in menu
+    m_createMenuAction->setShortcut(QKeySequence::New);
+    m_exitAction->setShortcut(QKeySequence::Quit);
+}
+
+void RecipePlanner::createMenu()
+{
+    // menu definition
+    m_fileMenu = menuBar()->addMenu("&File");
+
+    // action assignment
+    m_fileMenu->addAction(m_createMenuAction);
+    m_fileMenu->addAction(m_exitAction);
+}
--- lab1/recipe-planner/recipeplanner.h
+++ lab1/recipe-planner/recipeplanner.h
@@ -3,12 +3,47 @@
 
 #include <QMainWindow>
 
+#include <QListWidget>
+#include <QPushButton>
+
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+
+#include <QAction>
+#include <QMenu>
+#include <QMenuBar>
+
 class RecipePlanner : public QMainWindow
 {
     Q_OBJECT
 
+private:
+    QWidget *m_centralWidget;   // main widget
+    QListWidget *m_recipeList;  // main list with added recipes
+    QMenuBar *m_menuBar;        // menu bar
+
+    // buttons
+    QPushButton *m_addButton;
+    QPushButton *m_editButton;
+    QPushButton *m_deleteButton;
+
+    // layouts
+    QHBoxLayout *m_mainLayout;
+    QVBoxLayout *m_buttonLayout;
+
+    // actions and menu
+    QAction *m_createMenuAction;
+    QAction *m_exitAction;
+    QMenu *m_fileMenu;
+
+    // methods for constructor
+    void createButtons();
+    void createLayouts();
+    void createActions();
+    void createMenu();
+
 public:
     RecipePlanner(QWidget *parent = nullptr);
-    ~RecipePlanner();
+
 };
 #endif // RECIPEPLANNER_H
