a59583748d541ab7ce88ae2ae2e023c434dc950a
mmarcin5 <m.marcinkiewicz.5@stud.elka.pw.edu.pl>
2021-05-08 23:51:29 +0200

    Read JSON file and create models

--- lab2/recipe-planner/Controllers/HomeController.cs
+++ lab2/recipe-planner/Controllers/HomeController.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using recipe_planner.Models;
 
 namespace recipe_planner.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        // list of recipe models
+        public List<RecipeModel> recipes = new List<RecipeModel>();
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            ReadRecipeFile();
         }
 
         public IActionResult Index()
@@ -28,10 +29,62 @@ namespace recipe_planner.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        private void ReadRecipeFile()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            // read JSON file and deserialize it
+            var rawJson = System.IO.File.ReadAllText("recipes.json");
+            var deserializedJson = JsonSerializer.Deserialize<JsonElement>(rawJson);
+
+            // fill list of models in loop
+            foreach (var recipeJson in deserializedJson.EnumerateObject())
+            {
+                // create new recipe object and get the name of recipe
+                var newRecipe = new RecipeModel();
+                newRecipe.Name = recipeJson.Name;
+
+                // define lists with description and ingredients
+                newRecipe.Description = new List<string>();
+                newRecipe.Ingredients = new List<IngredientModel>();
+
+                // get content of recipe JSON object and get the rest information from JSON
+                var recipeContent = recipeJson.Value;
+                foreach (var recipeContentElement in recipeContent.EnumerateObject())
+                {
+                    // if name of property is recipe, then it is recipe's descritpion
+                    if (recipeContentElement.NameEquals("recipe"))
+                    {
+                        // add description to new recipe object
+                        foreach (var descriptionLine in recipeContentElement.Value.EnumerateArray())
+                            newRecipe.Description.Add(descriptionLine.GetString());
+                    }
+                    else
+                    {
+                        // create new ingredient object and get its name, quantity and unit
+                        var newIngredient = new IngredientModel();
+                        newIngredient.Name = recipeContentElement.Name;
+                        newIngredient.Quantity = CutQuantity(recipeContentElement.Value.GetString());
+                        newIngredient.Unit = CutUnit(recipeContentElement.Value.GetString());
+
+                        // add ingredient to the list
+                        newRecipe.Ingredients.Add(newIngredient);
+                    }
+                }
+
+                // add recipe to the list
+                recipes.Add(newRecipe);
+            }
+        }
+
+        // get quantity from string
+        private float CutQuantity(string qunatityWithUnit)
+        {
+            return Convert.ToSingle(qunatityWithUnit.Substring(0, qunatityWithUnit.IndexOf(' ')));
+        }
+
+        // get unit from string
+        private string CutUnit(string qunatityWithUnit)
+        {
+            return qunatityWithUnit.Substring(qunatityWithUnit.IndexOf(' ') + 1);
         }
     }
 }
